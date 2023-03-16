12626ec35d92def9e712799b44e80ea58ecd6b53
ETU\d.harle <damien.harle@gmail.com>
2023-03-16 16:30:37 +0100

    SelectAll w/o obstacle

--- BogeyMan/Assets/Scripts/Pour Julien/SelectAllObjectsWithoutColliders.cs
+++ BogeyMan/Assets/Scripts/Pour Julien/SelectAllObjectsWithoutColliders.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SelectAllObjectsWithoutColliders : MonoBehaviour
 {
     public static SelectAllObjectsWithoutColliders Instance = null;
     [SerializeField] internal List<GameObject> objectsWithoutColliders = new List<GameObject>();
+    [SerializeField] internal List<GameObject> objectsWithoutObstacleNavMesh = new List<GameObject>();
     
     private void Awake()
     {
@@ -26,17 +28,20 @@ public class SelectAllObjectsWithoutCollidersEditor : Editor
 {
     private SelectAllObjectsWithoutColliders selectAllObjectsWithoutColliders;
     private SerializedProperty objectsWithoutColliders;
+    private SerializedProperty objectsWithoutObstacleNavMesh;
 
     private void OnEnable()
     {
         selectAllObjectsWithoutColliders = (SelectAllObjectsWithoutColliders) target;
         objectsWithoutColliders = serializedObject.FindProperty("objectsWithoutColliders");
+        objectsWithoutObstacleNavMesh = serializedObject.FindProperty("objectsWithoutObstacleNavMesh");
     }
 
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
         EditorGUILayout.PropertyField(objectsWithoutColliders, true);
+        EditorGUILayout.PropertyField(objectsWithoutObstacleNavMesh, true);
         serializedObject.ApplyModifiedProperties();
         
         if (GUILayout.Button("Select All Objects Without Colliders"))
@@ -44,7 +49,11 @@ public class SelectAllObjectsWithoutCollidersEditor : Editor
             selectAllObjectsWithoutColliders.objectsWithoutColliders.Clear();
             foreach (GameObject gameObject in FindObjectsOfType<GameObject>())
             {
-                if (gameObject.GetComponent<Collider>() == null && gameObject.transform.parent != null && gameObject.transform.parent.GetComponent<Collider>() == null)
+                if (gameObject.GetComponent<NavMeshObstacle>() == null && gameObject.transform.parent != null && 
+                    gameObject.transform.parent.GetComponent<NavMeshObstacle>() == null
+                    && gameObject.gameObject.GetComponent<Player>() == null
+                    && gameObject.gameObject.GetComponent<NavMeshAgent>() == null
+                    && gameObject.GetComponent<Light>() == null)
                 {
                     selectAllObjectsWithoutColliders.objectsWithoutColliders.Add(gameObject);
                 }
@@ -58,6 +67,31 @@ public class SelectAllObjectsWithoutCollidersEditor : Editor
             selectAllObjectsWithoutColliders.objectsWithoutColliders.Clear();
             
             Selection.objects = selectAllObjectsWithoutColliders.objectsWithoutColliders.ToArray();
+        }
+        
+        if (GUILayout.Button("Select All Objects Without Obstacle NavMesh"))
+        {
+            selectAllObjectsWithoutColliders.objectsWithoutObstacleNavMesh.Clear();
+            foreach (GameObject gameObject in FindObjectsOfType<GameObject>())
+            {
+                if (gameObject.GetComponent<NavMeshObstacle>() == null && gameObject.transform.parent != null && 
+                    gameObject.transform.parent.GetComponent<NavMeshObstacle>() == null
+                    && gameObject.gameObject.GetComponent<Player>() == null
+                    && gameObject.gameObject.GetComponent<NavMeshAgent>() == null
+                    && gameObject.GetComponent<Light>() == null)
+                {
+                    selectAllObjectsWithoutColliders.objectsWithoutObstacleNavMesh.Add(gameObject);
+                }
+            }
+            
+            Selection.objects = selectAllObjectsWithoutColliders.objectsWithoutObstacleNavMesh.ToArray();
+        }
+        
+        if (GUILayout.Button("Deselect All Objects Without Obstacle NavMesh"))
+        {
+            selectAllObjectsWithoutColliders.objectsWithoutObstacleNavMesh.Clear();
+            
+            Selection.objects = selectAllObjectsWithoutColliders.objectsWithoutObstacleNavMesh.ToArray();
         }
     }
 }
